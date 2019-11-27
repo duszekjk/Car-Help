6d69960fe90b0b42f14d9a53ede2e46bcd0397bf
Jacek Kałużny <duszekjk@gmail.com>
2019-11-27 22:41:52 +0100

    Initial Commit

---
+++ CarHelp2/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  CarHelp2
+//
+//  Created by Jacek Kałużny on 27/11/2019.
+//  Copyright © 2019 Jacek Kałużny. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
